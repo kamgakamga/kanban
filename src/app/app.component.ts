f92e04aff948b68d1d56c8ca9878791bb5b7eed8
kamga serges <serge.kamga@iforce5.com>
2023-09-25 09:08:08 +0100

    debut de realisation du projet 6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
 import { Component } from '@angular/core';
 
+import { Task } from './task/task';
+import { CdkDrag, CdkDragDrop, CdkDropList, transferArrayItem} from '@angular/cdk/drag-drop';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+
 })
 export class AppComponent {
-  title = 'kanban-fire';
+  
+  todo : Task[] = [
+    {
+      title :'titre1',
+      description :'description1',
+    },
+    {
+      title :'titre2',
+      description :'description2',
+    },
+  ];
+
+  done : Task[] = [
+    {
+      title :'titre3',
+      description :'description3',
+    },
+    {
+      title :'titre4',
+      description :'description4',
+    },
+  ];
+  inProgress: Task[] = [];
+
+
+  editTask(list: string, task: Task): void {}
+
+  drop(event: CdkDragDrop<Task[]>): void {
+    if (event.previousContainer === event.container) {
+      return;
+    }
+    if (!event.container.data || !event.previousContainer.data) {
+      return;
+    }
+    transferArrayItem(
+      event.previousContainer.data,
+      event.container.data,
+      event.previousIndex,
+      event.currentIndex
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 import { AppComponent } from './app.component';
+import { TaskComponent } from './task/task.component';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, TaskComponent],
+  imports: [BrowserModule,MatToolbarModule,MatIconModule,MatCardModule,DragDropModule],
   providers: [],
   bootstrap: [AppComponent]
 })
